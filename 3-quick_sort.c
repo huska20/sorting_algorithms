8966633fa5a829184e471779c39b8d273e0035b8
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-06-26 12:49:45 -0700

    fix: change quick sort partition scheme

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,71 +2,62 @@
 #include <stdio.h>
 
 /**
- * swap - Swaps two integers.
- * @a: Pointer to the first integer.
- * @b: Pointer to the second integer.
- */
-
-void swap(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-	
-}
-
-/**
- * partition - Lomuto partition scheme for quicksort.
+ * divide_and_sort - Lomuto partition scheme for quicksort.
  * @array: The array to be sorted.
- * @low: Starting index of the partition.
- * @high: Ending index of the partition.
+ * @size: the size of the array for printing it.
+ * @start: Starting index of the partition.
+ * @end: Ending index of the partition.
  *
  * Return: The index of the pivot element.
  */
-int partition(int *array, int low, int high,size_t size)
+int divide_and_sort(int *array, size_t size, int start, int end)
 {
-	int pivot = array[high];
-	int i = low - 1;
-	int j;
+	int pivot, idx, j, temp;
 
-	for (j = low; j <= high - 1; j++)
+	pivot = array[end];
+	idx = start;
+
+	for (j = start; j < end; j++)
 	{
-		if (array[j] <= pivot)
+		if (array[j] < pivot)
 		{
-			i++;
-			swap(&array[i], &array[j]);
-			/*print_array(array, size);*/
+			if (array[idx] != array[j])
+			{
+				temp = array[idx];
+				array[idx] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+			idx++;
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
-	return (i + 1);
+	if (array[idx] != array[end])
+	{
+		temp = array[idx];
+		array[idx] = array[end];
+		array[end] = temp;
+		print_array(array, size);
+	}
+
+	return (idx);
 }
 
 /**
- * quick_sort_recursive - Recursive function to perform quicksort.
+ * quick_sort_recurse - Recursive function to perform quicksort.
  * @array: The array to be sorted.
- * @low: Starting index of the partition.
- * @high: Ending index of the partition.
+ * @size: the size of the array for printing it.
+ * @start: Starting index of the partition.
+ * @end: Ending index of the partition.
  */
-void quick_sort_recursive(int *array, int low, int high,size_t size)
+void quick_sort_recurse(int *array, size_t size, int start, int end)
 {
-	/*int i;*/
+	int p;
 
-	if (low < high)
+	if (start < end && end > 0) /* base case if false */
 	{
-		int pivot = partition(array, low, high, size);
-
-		/*printf("\n");
-		for (i = low; i <= high; i++)
-		{
-			printf("%d", array[i]);
-			if (i != high)
-				printf(", ");
-		}*/
-
-		quick_sort_recursive(array, low, pivot - 1, size);
-		quick_sort_recursive(array, pivot + 1, high, size);
+		p = divide_and_sort(array, size, start, end);
+		quick_sort_recurse(array, size, start, p - 1);
+		quick_sort_recurse(array, size, p + 1, end);
 	}
 }
 
@@ -80,5 +71,5 @@ void quick_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	quick_sort_recursive(array, 0, size - 1, size);
+	quick_sort_recurse(array, size, 0, size - 1);
 }
